9a7656a7a5a00fd7a5a43cb41712e548212f525e
杨尧 <yangyao@sailvan.com>
2019-04-13 16:04:31 +0800

    Add unit test for photo-classify

---
+++ tests/Unit/PhotoClassifyTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace PhotoClassifyTest\Unit;
+
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+use Yangyao\PhotoClassify\Command\ClassifyCommand;
+
+
+class PhotoClassifyTest extends TestCase
+{
+    /** @var $commandTester CommandTester */
+    private $commandTester;
+
+    protected function setUp()
+    {
+
+        $application = new Application();
+        $application->add(new ClassifyCommand());
+        $command = $application->find('app:run');
+        $this->commandTester = new CommandTester($command);
+
+    }
+
+    public function testPhotoClassify()
+    {
+        $source =  __DIR__.'/../resources/source/';
+        $target =  __DIR__.'/../resources/target/';
+        $this->commandTester->execute(['source' =>$source,'target'=>$target]);
+
+        $filename = $target.'2019-03/test-image.jpg';
+
+        $this->assertFileExists($filename);
+
+        @unlink($filename);
+        @rmdir($target.'2019-03');
+    }
+
+
+}
